87bc79a4072e2be77df81ef5fe4816ac24b44923
pedro.vinicius <pedro.vinicius@educ.rec.br>
2024-06-13 15:13:14 -0300

    Treinando Docker

---
+++ src/test/java/com/github/petervl80/springdocker/SpringdockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.petervl80.springdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
